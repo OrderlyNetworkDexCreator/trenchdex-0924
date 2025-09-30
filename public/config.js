cf7b5da54d0666bd7dcadaf43010b6ed27703bd9
Mario Reder <mario.reder@pm.me>
2025-09-30 12:55:23 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Portfolio,Trading,Markets,Leaderboard",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "TRADE $TRENCHDEX, https://dexscreener.com/solana/t4mbifahscjb9ngvggsynrkvc7a3a3c3477cufp2cf3",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
